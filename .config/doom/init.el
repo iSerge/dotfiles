e29a2abd6f430be45790cac327880a9fa742ecff
Serge Ivanov <lutvuk@gmail.com>
2022-05-14 10:52:40 +0300

    Disable dom eager native compilation

--- .config/doom/init.el
+++ .config/doom/init.el
@@ -1,5 +1,8 @@
 ;;; init.el -*- lexical-binding: t; -*-
 
+;; This disables eager native compilation of doom emacs
+(setq native-comp-deferred-compilation t)
+
 ;; This file controls what Doom modules are enabled and what order they load
 ;; in. Remember to run 'doom sync' after modifying it!
 
