0653e2901e83ebf866c315c9c48d0c682c1ab9e5
Serge Ivanov <lutvuk@gmail.com>
2023-07-18 14:06:25 +0300

    Add noice plugin

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -76,11 +76,26 @@ require('lazy').setup({
     },
     { 'nvim-lualine/lualine.nvim',
         dependencies = { 'nvim-tree/nvim-web-devicons', opt = true },
-        config = function() require('lualine').setup() end
+        config = function() require('lualine').setup({}) end
     },
     { 'lukas-reineke/indent-blankline.nvim' },
     { 'lewis6991/gitsigns.nvim',
         config = function () require('gitsigns').setup() end
+    },
+    {
+      "folke/noice.nvim",
+      event = "VeryLazy",
+      opts = {
+        -- add any options here
+      },
+      dependencies = {
+        -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+        "MunifTanjim/nui.nvim",
+        -- OPTIONAL:
+        --   `nvim-notify` is only needed, if you want to use the notification view.
+        --   If not available, we use `mini` as the fallback
+        "rcarriga/nvim-notify",
+        }
     },
 })
 
@@ -141,7 +156,21 @@ require('mason').setup({
     }
 })
 
-require('mason-lspconfig').setup()
+local lspconfig = require('lspconfig')
+local mason_lspconfig = require('mason-lspconfig')
+mason_lspconfig.setup()
+
+-- nvim-cmp supports additional completion capabilities, so broadcast that to servers
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
+
+mason_lspconfig.setup_handlers({
+    function(server)
+        lspconfig[server].setup({
+            capabilities = capabilities
+        })
+    end
+})
 
 -- LSP Diagnostics Options Setup 
 local sign = function(opts)
@@ -180,7 +209,7 @@ vim.diagnostic.config({
 -- updatetime: set updatetime for CursorHold
 vim.opt.completeopt = {'menuone', 'noselect', 'noinsert'}
 vim.opt.shortmess = vim.opt.shortmess + { c = true}
-vim.api.nvim_set_option('updatetime', 300) 
+vim.api.nvim_set_option('updatetime', 300)
 
 -- Fixed column for diagnostics to appear
 -- Show autodiagnostic popup on cursor hover_range
@@ -252,7 +281,7 @@ require('nvim-treesitter.configs').setup {
     enable = true,
     additional_vim_regex_highlighting=false,
   },
-  ident = { enable = true }, 
+  ident = { enable = true },
   rainbow = {
     enable = true,
     extended_mode = true,
@@ -273,4 +302,22 @@ vim.keymap.set('n', '<leader>fh', builtin.help_tags, {desc = 'Telescope help tag
 
 require('telescope').load_extension('fzf')
 
+require("noice").setup({
+  lsp = {
+    -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+    override = {
+      ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+      ["vim.lsp.util.stylize_markdown"] = true,
+      ["cmp.entry.get_documentation"] = true,
+    },
+  },
+  -- you can enable a preset for easier configuration
+  presets = {
+    bottom_search = true, -- use a classic bottom cmdline for search
+    command_palette = true, -- position the cmdline and popupmenu together
+    long_message_to_split = true, -- long messages will be sent to a split
+    inc_rename = false, -- enables an input dialog for inc-rename.nvim
+    lsp_doc_border = false, -- add a border to hover docs and signature help
+  },
+})
 
