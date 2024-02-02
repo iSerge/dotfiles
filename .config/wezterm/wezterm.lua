490e0f29cf766feb9a29cc545af65b490f3a2996
Serge Ivanov <lutvuk@gmail.com>
2024-02-02 15:20:36 +0300

    Configure some tab manipulation keys

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,4 +1,5 @@
 local wezterm = require 'wezterm'
+local act = wezterm.action
 
 local config = {}
 
@@ -19,7 +20,47 @@ config.font_size = 15.0
 config.initial_rows = 40
 config.initial_cols = 120
 
-print("Test")
+-- Activate tabs 1 to 8 with C+A+n or Fn keys
+config.keys = {}
+for i = 1, 8 do
+  -- CTRL+ALT + number to activate that tab
+  table.insert(config.keys, {
+    key = tostring(i),
+    mods = 'CTRL|ALT',
+    action = act.ActivateTab(i - 1),
+  })
+  -- F1 through F8 to activate that tab
+  table.insert(config.keys, {
+    key = 'F' .. tostring(i),
+    action = act.ActivateTab(i - 1),
+  })
+end
+
+-- Move between tabs with A+{ A+} keys
+config.keys = {
+  { key = '{', mods = 'ALT', action = act.ActivateTabRelative(-1) },
+  { key = '}', mods = 'ALT', action = act.ActivateTabRelative(1) },
+}
+
+-- Close current tab
+config.keys = {
+  {
+    key = 'w',
+    mods = 'CMD',
+    action = wezterm.action.CloseCurrentTab { confirm = true },
+  },
+}
+
+-- Create new tab
+config.keys = {
+  -- Create a new tab in the same domain as the current pane.
+  -- This is usually what you want.
+  {
+    key = 't',
+    mods = 'SHIFT|ALT',
+    action = act.SpawnTab 'CurrentPaneDomain',
+  },
+}
 
 return config
 
