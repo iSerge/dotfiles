8ca4cde6caf86c5202b2a5b263b36302f802ed14
Serge Ivanov <lutvuk@gmail.com>
2024-02-11 12:31:37 +0300

    Add ~/.local/share/nvim/mason/bin to PATH

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,3 @@
 set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME ; set -gx PATH $HOME/.cabal/bin /home/serge/.ghcup/bin $PATH # ghcup-env
 
-set -gx PATH ~/develop/flutter/sdk/bin ~/.local/neovim/bin ~/.local/bin ~/.config/emacs/bin ~/.cargo/bin $PATH
+set -gx PATH ~/develop/flutter/sdk/bin ~/.local/share/nvim/mason/bin ~/.local/neovim/bin ~/.local/bin ~/.config/emacs/bin ~/.cargo/bin $PATH
