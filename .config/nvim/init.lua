fe7cb5dbdcfb6a0a206cfcd359e626a1bfddb0c8
Serge Ivanov <lutvuk@gmail.com>
2024-02-22 15:57:28 +0300

    Update lua lsp config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -614,6 +614,7 @@ local servers = {
     Lua = {
       workspace = { checkThirdParty = false },
       telemetry = { enable = false },
+      diagnostics = { globals = { 'vim' } },
     },
   },
 }
