1d413ee6242f0a60de551b33c88d1c7a62856121
Serge Ivanov <lutvuk@gmail.com>
2025-04-11 12:45:01 +0300

    Add work scripts dir to path

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,7 @@
+
+set -gx PATH ~/.local/share/texlive/2024/bin/x86_64-linux ~/develop/work/scripts ~/develop/flutter/sdk/bin ~/.local/share/nvim/mason/bin ~/.local/neovim/bin ~/.local/bin ~/.config/emacs/bin ~/.cargo/bin $PATH
+
 set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME ; set -gx PATH $HOME/.cabal/bin /home/serge/.ghcup/bin $PATH # ghcup-env
 
-set -gx PATH ~/develop/flutter/sdk/bin ~/.local/share/nvim/mason/bin ~/.local/neovim/bin ~/.local/bin ~/.config/emacs/bin ~/.cargo/bin $PATH
+pyenv init - | source
+
